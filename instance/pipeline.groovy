c6eadfcda819c5aae13bcf11d8c3057c9f729d08
mayurwagh <mayurwagh1412@gmail.com>
2026-07-22 16:08:08 +0530

    terraform pipeline is added

--- instance/pipeline.groovy
+++ instance/pipeline.groovy
@@ -13,45 +13,44 @@ pipeline {
                     url: 'https://github.com/mayurmwagh/cdec-b52-53.git'
             }
         }
-
-        stage('Terraform Init') {
+        stage('Terraform-init') {
             steps {
-                    withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws_creds', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
-                
-                    
+                  withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws_creds', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
                     sh '''
                         export AWS_DEFAULT_REGION=$AWS_REGION
                         cd instance
                         terraform init
+                        
                     '''
+
                 }
             }
         }
-
-        stage('Terraform Plan') {
+        stage('Terraform-plan') {
             steps {
-                    withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws_creds', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
-
-                    sh '''
-                        export AWS_DEFAULT_REGION=$AWS_REGION
-                        cd instance                    
-                        terraform plan -out=tfplan
-                    '''
-                }
-            }
-        }
-
-        stage('Terraform Apply') {
-            steps {
-                    withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws_creds', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
-        
+                  withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws_creds', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
                     sh '''
                         export AWS_DEFAULT_REGION=$AWS_REGION
                         cd instance
-                        terraform apply -auto-approve tfplan
+                        terraform plan 
                     '''
+
                 }
             }
         }
+        stage('Terraform-Apply') {
+            steps {
+                  withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws_creds', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
+                    sh '''
+                        export AWS_DEFAULT_REGION=$AWS_REGION
+                        cd instance
+                        terraform apply --auto-approve
+                    '''
+
+                }
+            }
+        }
+
     }
+    
 }
